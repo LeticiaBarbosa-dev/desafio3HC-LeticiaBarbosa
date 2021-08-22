e89cbc9ae1a1ccc486665e7cc9a5449f50774e70
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2021-08-22 18:16:57 -0300

    Commit geral

---
+++ react/appHome.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+
+const appHome: FC = () => {
+  return <a href="https://upmedalhcleticiabarbosa--hiringcoders2021.myvtex.com/">
+    <img src="https://img.icons8.com/ios-glyphs/30/14ff00/home.png"/>
+  </a>
+}
+
+export default appHome
---
+++ react/appWhatsapp.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+
+const appWhatsapp: FC = () => {
+  return <a href="https://api.whatsapp.com/send?phone=5581989110636&text=Ol%C3%A1%2C%20voc%C3%AA%20poderiam%20me%20ajudar%3F">
+    <img src="https://img.icons8.com/material-outlined/30/14ff00/whatsapp--v1.png"/>
+    <h1></h1>
+  </a>
+}
+
+export default appWhatsapp
